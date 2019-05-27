4c0a3ec4c2e5010c2824f15d56c7ee3a6cea6bd0
Linnea <linneaj95@hotmail.com>
2019-05-27 10:10:37 +0200

    Grid x3 on admin

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -7,7 +7,7 @@ use Faker\Generator as Faker;
 
 $factory->define(Category::class, function (Faker $faker) {
     return [
-        'name' => $faker->name,
+        'name' => $faker->word,
         'category_id' => $faker->numberBetween($min=0, $max=4)
     ];
 });
